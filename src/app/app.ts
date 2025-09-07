c0b1a25d5d71e73167034b2a69aea09e5fefa117
Franklin Adae <frank.adae@gmail.com>
2025-09-06 20:59:55 -0400

    Finally got rid of service and did directly

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,45 @@
 import { Component, signal } from '@angular/core';
-import { PoetryDb } from './service/poetry-db';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  providers: [
-    {provide: 'peomTitle', useValue: 'poemTitle'},
-    {provide: 'author', useValue: 'author'}
-  ],
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('poet-database');
-  poemTitle="";
-  author="";
+
+  url="";
   poetryData:any;
 
-  constructor(private poetryService:PoetryDb){ }
+  constructor(private http:HttpClient){ }
 
-  getData(){
-              //What I wanted to do was pass in values entered and change url accordingly
-    this.poetryService.getPoetryData(/*this.poemTitle, this.author*/).subscribe((data:any)=>{
-      this.poetryData=data;
-    });
+  getData(poemTitle:string, author:string){
+    if(poemTitle.length == 0 && author.length > 0)
+    {
+      this.url="https://poetrydb.org/author/" + author + "/title,author,lines";
+    }
+    else if (poemTitle.length > 0 && author.length == 0)
+    {
+      this.url="https://poetrydb.org/title/" + poemTitle + "/title,author,lines";
+    }
+    else if (poemTitle.length > 0 && author.length > 0)
+    {
+      this.url="https://poetrydb.org/title,author/" + poemTitle + ";" + author + "/title,author,lines";
+    }
+    else
+    {
+      this.url="https://poetrydb.org/author/Emily Dickinson/title,author,lines"
+    }
+
+    this.poetryData = this.http.get(this.url);
+    this.poetryData.subscribe((response:any) => {
+        this.poetryData = response;
+        console.log('Data fetched:', this.poetryData);
+      },
+      (error:any) => {
+        console.error('Error fetching data:', error);
+      })
   }
 }
